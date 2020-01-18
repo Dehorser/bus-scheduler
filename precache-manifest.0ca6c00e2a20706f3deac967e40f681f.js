fad832b3c6fc9f7638f4f60f003d0847c364a32d
Joe Huang <happyjoe2008@gmail.com>
2020-01-18 00:57:55 -0800

    Updates

---
+++ precache-manifest.0ca6c00e2a20706f3deac967e40f681f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba0c198902822eb350c2efff239cc76",
+    "url": "/bus-scheduler/index.html"
+  },
+  {
+    "revision": "18af2098cf058e18c2cf",
+    "url": "/bus-scheduler/static/css/main.0d2a4a3e.chunk.css"
+  },
+  {
+    "revision": "4b08386a3e8ed7794d27",
+    "url": "/bus-scheduler/static/js/2.980ae448.chunk.js"
+  },
+  {
+    "revision": "ebffe31a042bb42599f5f5ad32635a48",
+    "url": "/bus-scheduler/static/js/2.980ae448.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18af2098cf058e18c2cf",
+    "url": "/bus-scheduler/static/js/main.f6ac88c5.chunk.js"
+  },
+  {
+    "revision": "c66570c73791e1d50201",
+    "url": "/bus-scheduler/static/js/runtime-main.ac9c18b5.js"
+  }
+]);
